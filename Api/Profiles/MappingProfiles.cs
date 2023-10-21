a6962374ae5e0ea6bcb0a75c0f44c26e5221a6a9
papashy <125417169+papashyy@users.noreply.github.com>
2023-10-20 22:01:54 -0500

    feat:

--- Api/Profiles/MappingProfiles.cs
+++ Api/Profiles/MappingProfiles.cs
@@ -7,27 +7,36 @@ using AutoMapper;
 using Dominio.Entities;
 // using Dominio.Views;
 
-namespace API.Profiles;
-    public class MappingProfiles : Profile
+namespace API.Profiles;    public class MappingProfiles : Profile
     {
         public MappingProfiles()
         {
-            CreateMap<Laboratorio,LabDto>().ReverseMap();
-            CreateMap<Medicamento,MedicamentoDto>().ReverseMap();
-            CreateMap<Veterinario,VetDto>().ReverseMap();
-            CreateMap<Mascota,PetDto>().ReverseMap();
-            CreateMap<Mascota,PetxBreedDto>().ReverseMap();
-            CreateMap<Raza,BreedDto>().ReverseMap();
-            CreateMap<Especie,SpeciesDto>().ReverseMap();
-            CreateMap<Propietario,OwnerDto>().ReverseMap();
-            CreateMap<Propietario,OwnerxPetsDto>().ReverseMap();
-            CreateMap<Raza,BreedxPetsDto>().ReverseMap();
-            CreateMap<Especie,SpeciesxPetDto>().ReverseMap();
-            CreateMap<Cita,AppointmentxPetDto>().ReverseMap();
-            CreateMap<Proveedor,ProviderDto>().ReverseMap();
-            CreateMap<Usuario,UsuarioDto>().ReverseMap();
-            CreateMap<MovimientoTotal,TotalMovimientoDto>().ReverseMap();
-            CreateMap<Cita,AppointmentDto>().ReverseMap();
-            CreateMap<MovimientoMedicamento,MovimientoDto>().ReverseMap();
+            CreateMap<Cargo, CargoDto>().ReverseMap();
+            CreateMap<Cliente, ClienteDto>().ReverseMap();
+            CreateMap<Color, ColorDto>().ReverseMap();
+            CreateMap<Departamento, DepartamentoDto>().ReverseMap();
+            CreateMap<DetalleOrden, DetalleOrdenDto>().ReverseMap();
+            CreateMap<DetalleVenta, DetalleVentaDto>().ReverseMap();
+            CreateMap<Empleado, EmpleadoDto>().ReverseMap();
+            CreateMap<Empresa, EmpresaDto>().ReverseMap();
+            CreateMap<Estado, EstadoDto>().ReverseMap();
+            CreateMap<FormaPago, FormaPagoDto>().ReverseMap();
+            CreateMap<Genero, GeneroDto>().ReverseMap();
+            CreateMap<Insumo, InsumoDto>().ReverseMap();
+            CreateMap<InsumoPrenda, InsumoPrendaDto>().ReverseMap();
+            CreateMap<Inventario, InventarioDto>().ReverseMap();
+            CreateMap<InventarioTalla, InventarioTallaDto>().ReverseMap();
+            CreateMap<Municipio, MunicipioDto>().ReverseMap();
+            CreateMap<Orden, OrdenDto>().ReverseMap();
+            CreateMap<Pais, PaisDto>().ReverseMap();
+            CreateMap<Prenda, PrendaDto>().ReverseMap();
+            CreateMap<Proveedor, ProveedorDto>().ReverseMap();
+            // CreateMap<Rol, RolDto>().ReverseMap();
+            CreateMap<Talla, TallaDto>().ReverseMap();
+            CreateMap<TipoEstado, TipoEstadoDto>().ReverseMap();
+            CreateMap<TipoPersona, TipoPersonaDto>().ReverseMap();
+            CreateMap<TipoProteccion, TipoProteccionDto>().ReverseMap();
+            CreateMap<Venta, VentaDto>().ReverseMap();
+
         }
     }
